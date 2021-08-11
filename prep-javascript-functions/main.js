fc787a475b1af1ec9c57518b84f0ff9ed537b814
Jeff Kwon <jykwon007@gmail.com>
2021-08-10 20:58:55 -0700

    Completed addTwoNumbers function.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+
+console.log('addTwoNumbers(2, 2): ', addTwoNumbersResult);
