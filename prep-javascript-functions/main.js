79e9152e52d850ebcab299a0b821441f8eaaf236
Jeff Kwon <jykwon007@gmail.com>
2021-08-10 21:03:00 -0700

    Completed convertHoursToMinutes function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -6,5 +6,11 @@ var sum = addTwoNumbers(4, 4);
 console.log('sum', sum);
 
 var addTwoNumbersResult = addTwoNumbers(2, 2);
-
 console.log('addTwoNumbers(2, 2): ', addTwoNumbersResult);
+
+function convertHoursToMinutes(x) {
+  return x * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes(2):', convertHoursToMinutesResult);
